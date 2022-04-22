8cd9ec893e35d5c6e68274d94814cc76065091b3
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2022-04-22 14:18:27 +0900

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,16 +5,18 @@
     <title>ログアウト</title>
 </head>
 <body>
-    <?php
-         //クロスサイトリクエストフォージェリ（CSRF）対策
-             $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
-             $token = $_SESSION['token'];
-         //クリックジャッキング対策
-             header('X-FRAME-OPTIONS: SAMEORIGIN'); 
+
+<?php
+    session_start();
+    //クロスサイトリクエストフォージェリ（CSRF）対策
+    $_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
+    $token = $_SESSION['token'];
+    //クリックジャッキング対策
+    header('X-FRAME-OPTIONS: SAMEORIGIN'); 
              
-         $_SESSION = array();
-         @session_destroy();
-    ?>
+    session_destroy();
+?>
+
 <p>ログアウトしました。</p>
 <a href = "m6-login-form.php">ログインへ</a>
 </body>
